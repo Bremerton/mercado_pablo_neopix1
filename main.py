50444a2f8b6beee0be514f17ba026160fb16f211
Bremerton <72746036+Bremerton@users.noreply.github.com>
2020-10-20 11:24:46 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+light.set_pixel_color(0, light.rgb(255,165, 0))
+light.set_pixel_color(1, light.rgb(173, 255, 47))
+light.set_pixel_color(2, light.rgb(0, 255, 0))
+light.set_pixel_color(3, light.rgb(0, 255, 190))
+light.set_pixel_color(4, light.rgb(10, 158, 255))
+light.set_pixel_color(5, light.rgb(0, 110, 255))
+light.set_pixel_color(6, light.rgb(8, 78, 255))
+light.set_pixel_color(7, light.rgb(147, 112, 219))
+light.set_pixel_color(8, light.rgb(255, 0, 255))
+light.set_pixel_color(9, light.rgb(255, 20, 147))
--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+light.setPixelColor(0, light.rgb(255, 165, 0))
+light.setPixelColor(1, light.rgb(173, 255, 47))
+light.setPixelColor(2, light.rgb(0, 255, 0))
+light.setPixelColor(3, light.rgb(0, 255, 190))
+light.setPixelColor(4, light.rgb(10, 158, 255))
+light.setPixelColor(5, light.rgb(0, 110, 255))
+light.setPixelColor(6, light.rgb(8, 78, 255))
+light.setPixelColor(7, light.rgb(147, 112, 219))
+light.setPixelColor(8, light.rgb(255, 0, 255))
+light.setPixelColor(9, light.rgb(255, 20, 147))
